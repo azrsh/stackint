d35a9c2ebf450098d29b4048631e1b021e569cbf
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2021-07-17 13:47:04 +0900

    add call

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 mod vm;
-use crate::vm;
-use std::collections::HashMap;
-use std::collections::VecDeque;
 use std::env;
 use std::fs;
 use std::io;
-use std::process;
-
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,65 +10,23 @@ fn main() {
 
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
-    interpret(&mut stdout, contents);
-}
-
-fn interpret<W: io::Write>(w: &mut W, source: String) {
-    let mut stack = VecDeque::<i32>::new();
-    let mut variables = HashMap::<String, i32>::new();
-
-    let lines = source.split("\n").collect::<Vec<&str>>();
-    let mut line_num = 0;
-    while line_num < lines.len() {
-        let line = lines[line_num];
-        line_num += 1;
-
-        let line: Vec<&str> = line.trim_end().split(" ").collect();
-
-        if line[0] == "push" {
-            let i = line[1].parse::<i32>().unwrap();
-            vm::push(&mut stack, i);
-        } else if line[0] == "pop" {
-            vm::pop(&mut stack);
-        } else if line[0] == "jump" {
-            let i = line[1].parse::<i32>().unwrap();
-            vm::jump(&mut line_num, i);
-        } else if line[0] == "jumpif" {
-            let i = line[1].parse::<i32>().unwrap();
-            vm::jumpif(&mut stack, &mut line_num, i);
-        } else if line[0] == "add" {
-            vm::add(&mut stack);
-        } else if line[0] == "sub" {
-            vm::sub(&mut stack);
-        } else if line[0] == "mul" {
-            vm::mul(&mut stack);
-        } else if line[0] == "set" {
-            vm::set(&mut stack, &mut variables, line[1].to_string());
-        } else if line[0] == "get" {
-            vm::get(&mut stack, &variables, line[1].to_string());
-        } else if line[0] == "print" {
-            vm::print(&mut stack, w);
-        } else if line[0] == "halt" {
-            vm::halt();
-        } else {
-            panic!("Unkown code");
-        }
-    }
+    vm::interpret(&mut stdout, contents);
 }
 
 #[cfg(test)]
 mod tests {
-use crate::interpret;
+    use crate::vm;
     #[test]
     fn add() {
         let mut buf = Vec::<u8>::new();
-        interpret(
+        vm::interpret(
             &mut buf,
             "push 1\n\
             push 2\n\
             add\n\
-            print\n\
-            halt\n".to_string(),
+            call 0\n\
+            halt\n"
+                .to_string(),
         );
         assert_eq!(buf, b"3\n");
     }
--- src/vm.rs
+++ src/vm.rs
@@ -1,9 +1,20 @@
 use std::collections::HashMap;
 use std::collections::VecDeque;
-use std::env;
-use std::fs;
 use std::io;
 use std::process;
+
+enum Function<'a, W: io::Write> {
+    Native(
+        fn(
+            w: &mut W,
+            stack: &mut VecDeque<i32>,
+            function_history: &mut VecDeque<usize>,
+            variables: &mut HashMap<String, i32>,
+            function_table: &Vec<Function<W>>,
+        ),
+    ),
+    Source(&'a str),
+}
 
 fn push(stack: &mut VecDeque<i32>, i: i32) {
     stack.push_back(i);
@@ -47,4 +58,107 @@ fn set(stack: &mut VecDeque<i32>, variables: &mut HashMap<String, i32>, name: St
 
 fn get(stack: &mut VecDeque<i32>, variables: &HashMap<String, i32>, name: String) {
     push(stack, variables[&name]);
+}
+
+fn call<W: io::Write>(
+    w: &mut W,
+    stack: &mut VecDeque<i32>,
+    function_history: &mut VecDeque<usize>,
+    variables: &mut HashMap<String, i32>,
+    function_table: &Vec<Function<W>>,
+    function_id: usize,
+) {
+    function_history.push_back(function_id);
+    interpret_function(w, stack, function_history, variables, function_table)
+}
+
+fn ret(function_history: &mut VecDeque<usize>) {
+    function_history.pop_back().unwrap();
+}
+
+fn halt() {
+    process::exit(0);
+}
+
+fn interpret_function<W: io::Write>(
+    w: &mut W,
+    stack: &mut VecDeque<i32>,
+    function_history: &mut VecDeque<usize>,
+    variables: &mut HashMap<String, i32>,
+    function_table: &Vec<Function<W>>,
+) {
+    match function_table[*function_history.back().unwrap()] {
+        Function::Native(function) => function(w, stack, function_history, variables, function_table),
+        Function::Source(source) => {
+            let lines = source.split("\n").collect::<Vec<&str>>();
+            let mut line_num = 0;
+            while line_num < lines.len() {
+                let line = lines[line_num];
+                line_num += 1;
+
+                let line: Vec<&str> = line.trim_end().split(" ").collect();
+
+                if line[0] == "push" {
+                    let i = line[1].parse::<i32>().unwrap();
+                    push(stack, i);
+                } else if line[0] == "pop" {
+                    pop(stack);
+                } else if line[0] == "jump" {
+                    let i = line[1].parse::<i32>().unwrap();
+                    jump(&mut line_num, i);
+                } else if line[0] == "jumpif" {
+                    let i = line[1].parse::<i32>().unwrap();
+                    jumpif(stack, &mut line_num, i);
+                } else if line[0] == "add" {
+                    add(stack);
+                } else if line[0] == "sub" {
+                    sub(stack);
+                } else if line[0] == "mul" {
+                    mul(stack);
+                } else if line[0] == "set" {
+                    set(stack, variables, line[1].to_string());
+                } else if line[0] == "get" {
+                    get(stack, variables, line[1].to_string());
+                } else if line[0] == "call" {
+                    let id = line[1].parse::<usize>().unwrap();
+                    call(w, stack, function_history, variables, function_table, id);
+                } else if line[0] == "ret" {
+                    ret(function_history);
+                    return;
+                } else if line[0] == "halt" {
+                    halt();
+                } else {
+                    panic!("Unkown code");
+                }
+            }
+        }
+    }
+}
+
+pub fn interpret<W: io::Write>(w: &mut W, source: String) {
+    let mut stack = VecDeque::<i32>::new();
+    let mut function_history = VecDeque::<usize>::new();
+    let mut variables = HashMap::<String, i32>::new();
+    let mut function_table = Vec::<Function<W>>::new();
+
+    function_table.push(Function::Native(
+        |w: &mut W,
+         stack: &mut VecDeque<i32>,
+         _function_history: &mut VecDeque<usize>,
+         _variables: &mut HashMap<String, i32>,
+         _function_table: &Vec<Function<W>>| {
+            let i = *stack.back().unwrap();
+            writeln!(w, "{}", i).unwrap();
+        },
+    ));
+    function_table.push(Function::Source(&source));
+
+    call(
+        w,
+        &mut stack,
+        &mut function_history,
+        &mut variables,
+        &function_table,
+        1,
+    );
 }
