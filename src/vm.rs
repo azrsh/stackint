34b37a1a67aacdfe429b3b6f6d3aeea098190fb3
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2021-07-17 14:47:13 +0900

    move function match

--- src/vm.rs
+++ src/vm.rs
@@ -62,7 +62,10 @@ fn get(stack: &mut VecDeque<i32>, variables: &HashMap<String, i32>, name: String
 
 fn call<W: io::Write>(ctx: &mut RuntimeContext<W>, function_id: u32) {
     ctx.function_history.push_back(function_id);
-    interpret_function(ctx)
+    match ctx.function_table[ctx.function_history.back().unwrap()] {
+        Function::Native(function) => function(ctx),
+        Function::Source(source) => interpret_function(ctx, source),
+    }
 }
 
 fn ret(function_history: &mut VecDeque<u32>) {
@@ -73,51 +76,46 @@ fn halt() {
     process::exit(0);
 }
 
-fn interpret_function<W: io::Write>(ctx: &mut RuntimeContext<W>) {
-    match ctx.function_table[ctx.function_history.back().unwrap()] {
-        Function::Native(function) => function(ctx),
-        Function::Source(source) => {
-            let lines = source.split('\n').collect::<Vec<&str>>();
-            let mut line_num = 0;
-            while line_num < lines.len() {
-                let line = lines[line_num];
-                line_num += 1;
+fn interpret_function<W: io::Write>(ctx: &mut RuntimeContext<W>, source: &str) {
+    let lines = source.split('\n').collect::<Vec<&str>>();
+    let mut line_num = 0;
+    while line_num < lines.len() {
+        let line = lines[line_num];
+        line_num += 1;
 
-                let line: Vec<&str> = line.trim_end().split(' ').collect();
+        let line: Vec<&str> = line.trim_end().split(' ').collect();
 
-                if line[0] == "push" {
-                    let i = line[1].parse::<i32>().unwrap();
-                    push(ctx.stack, i);
-                } else if line[0] == "pop" {
-                    pop(ctx.stack);
-                } else if line[0] == "jump" {
-                    let i = line[1].parse::<i32>().unwrap();
-                    jump(&mut line_num, i);
-                } else if line[0] == "jumpif" {
-                    let i = line[1].parse::<i32>().unwrap();
-                    jumpif(ctx.stack, &mut line_num, i);
-                } else if line[0] == "add" {
-                    add(ctx.stack);
-                } else if line[0] == "sub" {
-                    sub(ctx.stack);
-                } else if line[0] == "mul" {
-                    mul(ctx.stack);
-                } else if line[0] == "set" {
-                    set(ctx.stack, ctx.variables, line[1].to_string());
-                } else if line[0] == "get" {
-                    get(ctx.stack, ctx.variables, line[1].to_string());
-                } else if line[0] == "call" {
-                    let id = line[1].parse::<u32>().unwrap();
-                    call(ctx, id);
-                } else if line[0] == "ret" {
-                    ret(ctx.function_history);
-                    return;
-                } else if line[0] == "halt" {
-                    halt();
-                } else {
-                    panic!("Unkown code");
-                }
-            }
+        if line[0] == "push" {
+            let i = line[1].parse::<i32>().unwrap();
+            push(ctx.stack, i);
+        } else if line[0] == "pop" {
+            pop(ctx.stack);
+        } else if line[0] == "jump" {
+            let i = line[1].parse::<i32>().unwrap();
+            jump(&mut line_num, i);
+        } else if line[0] == "jumpif" {
+            let i = line[1].parse::<i32>().unwrap();
+            jumpif(ctx.stack, &mut line_num, i);
+        } else if line[0] == "add" {
+            add(ctx.stack);
+        } else if line[0] == "sub" {
+            sub(ctx.stack);
+        } else if line[0] == "mul" {
+            mul(ctx.stack);
+        } else if line[0] == "set" {
+            set(ctx.stack, ctx.variables, line[1].to_string());
+        } else if line[0] == "get" {
+            get(ctx.stack, ctx.variables, line[1].to_string());
+        } else if line[0] == "call" {
+            let id = line[1].parse::<u32>().unwrap();
+            call(ctx, id);
+        } else if line[0] == "ret" {
+            ret(ctx.function_history);
+            return;
+        } else if line[0] == "halt" {
+            halt();
+        } else {
+            panic!("Unkown code");
         }
     }
 }
